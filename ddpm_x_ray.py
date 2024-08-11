0a8c3d90c66de2e62efc0e448190e2e5ad652850
Leonardo Holtz de Oliveira <leonardo.holtz@senairs.org.br>
2024-08-10 21:14:17 -0300

    fix: Change deprecated autocast and fix sampling problem

--- ddpm_x_ray.py
+++ ddpm_x_ray.py
@@ -20,7 +20,7 @@ from monai.apps import MedNISTDataset
 from monai.config import print_config
 from monai.data import CacheDataset, DataLoader, CSVDataset, PersistentDataset, pad_list_data_collate
 from monai.utils import first, set_determinism
-from torch.cuda.amp import GradScaler, autocast
+from torch.amp import GradScaler, autocast
 from tqdm import tqdm
 
 from generative.inferers import DiffusionInferer
@@ -229,7 +229,7 @@ def main():
                 
                 optimizer.zero_grad(set_to_none=True)
 
-                with autocast(enabled=True):
+                with autocast(device_type="cuda", enabled=True):
                     # Generate random noise
                     noise = torch.randn_like(images).to(device)
 
@@ -271,7 +271,7 @@ def main():
                     images = batch[0].to(device)
                     labels = batch[1].to(device)
                     with torch.no_grad():
-                        with autocast(enabled=True):
+                        with autocast(device_type="cuda", enabled=True):
                             noise = torch.randn_like(images).to(device)
                             timesteps = torch.randint(
                                 0, inferer.scheduler.num_train_timesteps, (images.shape[0],), device=images.device
@@ -289,22 +289,21 @@ def main():
                 scheduler.set_timesteps(num_inference_steps=1000)
                 label_0 = torch.tensor([0]).to(device)
                 label_1 = torch.tensor([1]).to(device)
-                with autocast(enabled=True):
+                with autocast(device_type="cuda", enabled=True):
+                    print(f"\n====== SAMPLING IMAGES IN EPOCH {epoch} ======")
                     image_0 = inferer.sample(input_noise=noise, diffusion_model=model, class_label=label_0, scheduler=scheduler)
                     image_1 = inferer.sample(input_noise=noise, diffusion_model=model, class_label=label_1, scheduler=scheduler)
 
-                plt.figure(figsize=(2, 2))
+                plt.figure()
                 plt.imshow(image_0[0, 0].cpu(), vmin=0, vmax=1, cmap="gray")
+                plt.axis('off')
                 plt.tight_layout()
-                plt.axis("off")
-                plt.show()
                 plt.savefig(f"sample_class_0_epoch_{epoch}.jpg")
                 
-                plt.figure(figsize=(2, 2))
+                plt.figure()
                 plt.imshow(image_1[0, 0].cpu(), vmin=0, vmax=1, cmap="gray")
+                plt.axis('off')
                 plt.tight_layout()
-                plt.axis("off")
-                plt.show()
                 plt.savefig(f"sample_class_1_epoch_{epoch}.jpg")
 
         total_time = time.time() - total_start
