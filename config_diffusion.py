86a7cdef2446d4719e1a23ed8080868b78d3975c
Leonardo Holtz <leo.holtz98@gmail.com>
2024-08-11 22:31:14 -0300

    Create config_diffusion.py

---
+++ config_diffusion.py
@@ -0,0 +1,20 @@
+# Training Hyperparameters
+INPUT_SIZE = 784 # 784 for mnist, 65536 for xray
+NUM_CLASSES = 2
+LEARNING_RATE = 0.0001
+NUM_EPOCHS = 25
+N_FEATURES = 128 # 128 ok, 256 better (but slower)
+BETAS = (1e-4, 0.02)
+N_T = 500
+
+# Dataset
+DATA_DIR = "AiresPucrs/chest-xray"
+NUM_WORKERS = 8
+BATCH_SIZE = 32
+
+# Compute related
+ACCELERATOR = "gpu"
+DEVICES = [0]
+PRECISION = 32
+
+MODEL_CHECKPOINT_DIR = "./checkpoints/"
